8ed242cfeea97ee3e543ceae9b87c3abd3559f4d
zujiawei <928038402@qq.com>
2019-06-28 18:04:44 +0800

    123

--- src/components/views/formsumbit.vue
+++ src/components/views/formsumbit.vue
@@ -2,41 +2,41 @@
   <el-form :label-position="labelPosition" ref="form" :model="form" label-width="80px">
     <el-form-item label="司机姓名:">
       <el-select v-model="form.name" placeholder="请选择">
-        <el-option
-          v-for="item in nameOptions"
-          :key="item"
-          :label="item"
-          :value="item">
+        <el-option itemid="1"
+                   v-for="item in nameOptions"
+                   :key="item + '-1'"
+                   :label="item"
+                   :value="item">
         </el-option>
       </el-select>
     </el-form-item>
     <el-form-item label="车号:">
       <el-select v-model="form.carNum" placeholder="请选择">
-        <el-option
-          v-for="item in carNumberOptions"
-          :key="item"
-          :label="item"
-          :value="item">
+        <el-option itemid="2"
+                   v-for="item in carNumberOptions"
+                   :key="item + '-2'"
+                   :label="item"
+                   :value="item">
         </el-option>
       </el-select>
     </el-form-item>
     <el-form-item label="库区">
       <el-select v-model="form.area" placeholder="请选择">
-        <el-option
-          v-for="item in wareHousesOptions"
-          :key="item"
-          :label="item"
-          :value="item">
+        <el-option itemid="3"
+                   v-for="item in wareHousesOptions"
+                   :key="item + '-3'"
+                   :label="item"
+                   :value="item">
         </el-option>
       </el-select>
     </el-form-item>
     <el-form-item label="大架号:">
       <el-select v-model="form.frameNum" placeholder="请选择">
-        <el-option
-          v-for="item in shelvesOptions"
-          :key="item"
-          :label="item"
-          :value="item">
+        <el-option itemid="4"
+                   v-for="item in shelvesOptions"
+                   :key="item + '-4'"
+                   :label="item"
+                   :value="item">
         </el-option>
       </el-select>
     </el-form-item>
@@ -50,8 +50,9 @@
 </template>
 <script>
   import axios from 'axios'
+
   export default {
-    data () {
+    data() {
       return {
         nameOptions: [],
         carNumberOptions: [],
@@ -63,18 +64,18 @@
           carNum: '',
           area: '',
           frameNum: '',
-          startDate: '',
-        }
+          startDate: ''
+        },
       }
     },
-    created () {
+    created() {
       this.getDriverName()
     },
     methods: {
-      async getDriverName () {
+      async getDriverName() {
         axios.get('api/from/getInfos')
           .then((response) => {
-            let res =response.data;
+            let res = response.data;
             let driver = res.drivers;
             let carNumbers = res.carNumbers;
             let shelves = res.shelves;
@@ -100,8 +101,53 @@
             console.log(error)
           })
       },
-      onSubmit () {
-        console.log('submit!')
+      onSubmit() {
+        let name = this.form.name
+        let carNum = this.form.carNum
+        let area = this.form.area
+        let frameNum = this.form.frameNum
+        let startDate = this.form.startDate
+        this.$refs.form.validate((valid) => {
+          if (valid) {
+            if (name == null) {
+              this.$Notice.destroy()
+              this.$Notice.warning({
+                title: '司机姓名不能为空',
+                duration: 2
+              })
+            } else if (area == null) {
+              this.$Notice.destroy()
+              this.$Notice.warning({
+                title: '库房号不能为空',
+                duration: 2
+              })
+            } else if (frameNum == null) {
+              this.$Notice.destroy()
+              this.$Notice.warning({
+                title: '大架号不能为空',
+                duration: 2
+              })
+            } else if (carNum == null) {
+              this.$Notice.destroy()
+              this.$Notice.warning({
+                title: '车号不能为空',
+                duration: 2
+              })
+            } else if (startDate == null) {
+              this.$Notice.destroy()
+              this.$Notice.warning({
+                title: '起始时间不能为空',
+                duration: 2
+              })
+            } else {
+              let postObj = {
+                name: this.formItem.name,
+                resources: useList
+              }
+              this.add(postObj)
+            }
+          }
+        })
       }
     },
   }
